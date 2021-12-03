f6931a0cc62bbdd4a14cabe6e19ac67fdc705b11
sachin sah <sachin.barakar@gmail.com>
2021-12-03 13:28:05 -0800

    second commit

--- prac1/testapp/views.py
+++ prac1/testapp/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def display(request):
-    return HttpResponse('<h1>demo for practicing git</h1>')
+    s='<h1>demo for practicing git</h1>'
+    return HttpResponse(s)
+    
